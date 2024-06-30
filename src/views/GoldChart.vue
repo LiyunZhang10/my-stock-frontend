4db45ff2306b8c240ad1f4acbdafd92e7a06dd14
Neymarz1y0 <743478304@qq.com>
2024-06-30 14:11:24 +0800

    黄金界面

--- src/views/GoldChart.vue
+++ src/views/GoldChart.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    <div ref="goldChart" style="width: 100%; height: 600px;"></div>
+  <div class="min-h-screen bg-gray-100 p-4">
+    <el-card class="w-full h-full shadow-lg">
+      <template #header>
+        <div class="flex justify-between items-center">
+          <span class="text-3xl font-bold text-gray-800"
+            >金价和变化率趋势图</span
+          >
+          <el-button
+            type="primary"
+            icon="el-icon-back"
+            @click="$router.push('/home')"
+            class="bg-blue-500 hover:bg-blue-600"
+            >返回首页</el-button
+          >
+        </div>
+      </template>
+      <div
+        ref="goldChart"
+        style="width: 100%; height: 600px"
+        class="w-full h-[600px]"></div>
+      <p class="text-sm text-gray-500 mt-4 text-center">该数据来源自东方财富</p>
+    </el-card>
   </div>
 </template>
 
@@ -16,17 +36,19 @@ export default {
     const goldChartData = ref({
       labels: [],
       prices: [],
-      changeRates: []
+      changeRates: [],
     });
 
     const fetchGoldData = async () => {
       try {
-        const goldResponse = await axios.get('http://localhost:8080/api/latest-gold-data');
+        const goldResponse = await axios.get(
+          'http://localhost:8080/api/latest-gold-data'
+        );
         const data = goldResponse.data;
 
-        goldChartData.value.labels = data.map(item => item.timestamp);
-        goldChartData.value.prices = data.map(item => item.price);
-        goldChartData.value.changeRates = data.map(item => item.changeRate);
+        goldChartData.value.labels = data.map((item) => item.timestamp);
+        goldChartData.value.prices = data.map((item) => item.price);
+        goldChartData.value.changeRates = data.map((item) => item.changeRate);
 
         updateChart();
       } catch (error) {
@@ -40,30 +62,30 @@ export default {
         const option = {
           title: {
             text: '金价和变化率',
-            left: 'center'
+            left: 'center',
           },
           tooltip: {
-            trigger: 'axis'
+            trigger: 'axis',
           },
           legend: {
             data: ['金价', '变化率'],
-            left: 'left'
+            left: 'left',
           },
           xAxis: {
             type: 'category',
-            data: goldChartData.value.labels
+            data: goldChartData.value.labels,
           },
           yAxis: [
             {
               type: 'value',
               name: '金价',
-              position: 'left'
+              position: 'left',
             },
             {
               type: 'value',
               name: '变化率',
-              position: 'right'
-            }
+              position: 'right',
+            },
           ],
           series: [
             {
@@ -73,8 +95,8 @@ export default {
               yAxisIndex: 0,
               smooth: true,
               lineStyle: {
-                color: '#f9d423'
-              }
+                color: '#f9d423',
+              },
             },
             {
               name: '变化率',
@@ -83,24 +105,24 @@ export default {
               yAxisIndex: 1,
               smooth: true,
               lineStyle: {
-                color: '#e14eca'
-              }
-            }
-          ]
+                color: '#e14eca',
+              },
+            },
+          ],
         };
         chart.setOption(option);
       }
     };
 
-    onMounted(() => {
-      fetchGoldData();
-      setInterval(fetchGoldData, 6000);
+    onMounted(async () => {
+      await fetchGoldData();
+      setInterval(fetchGoldData, 600000);
     });
 
     return {
-      goldChart
+      goldChart,
     };
-  }
+  },
 };
 </script>
 
