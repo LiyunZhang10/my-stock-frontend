83b3c06e3ff9f6af4c611e83a91eee8f489e4b98
zhangliyun <743478304@qq.com>
2025-01-02 17:50:19 +0800

    转发配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-
+// vue.config.js
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
@@ -14,5 +14,14 @@ module.exports = defineConfig({
   devServer: {
     port: 8083,
     allowedHosts: 'all',
+    proxy: {
+      '/api': {
+        // target: 'http://localhost:8080', // 后端 API 地址
+        target: 'http://zhangliyun10.gnway.cc:8000',
+        changeOrigin: true, // 修改请求头中的来源字段
+        secure: false, // 如果使用 https，请设置为 true
+        logLevel: 'debug', // 输出代理请求的详细日志，帮助调试
+      },
+    },
   },
-})
+});
